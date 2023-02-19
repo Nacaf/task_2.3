3874e0d4535fad38e3105bff67f4662e2c119bf4
Najaf Sharifov <nwerifov@mail.ru>
2023-02-19 21:40:30 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+//Task 2.3
+/*let a=1, b=2, c=3, d=4;
+let sum = a + b + c + d;
+console.log(sum);*/
+
+/*let fourDigitNum = prompt("Enter four-digit number");
+let num =fourDigitNum.split("");
+let sum = +num[0] + +num[1] + +num[2] + +num[3];
+console.log(sum);
+console.log(`"Ədədin rəqəmlərinin cəmi ${sum} - dir"`);*/
+
+
+
+//Task 2.3 Bonus
+
+/*let fullName = prompt("Enter full name");
+let user = fullName.split(" ");
+let name = user[0];
+let surName = user[1];
+console.log(`Salam, ${user[0]}`);*/
